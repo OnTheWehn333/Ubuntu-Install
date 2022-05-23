957090b198912974dc94f4409110e46361af420c
noahbalboa66 <noahwehn@gmail.com>
2022-05-22 23:18:52 -0500

    Change ohmyzsh install to unattended and after change the default shell

--- installZsh.sh
+++ installZsh.sh
@@ -7,10 +7,14 @@ sudo apt install curl
 echo "Installing zsh"
 sudo apt install zsh
 echo "Installing ohmyzsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "Installing starship"
 sudo apt install fonts-powerline
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 echo "adding eval \"$(starship init zsh)\" to .zshrc"
 echo -e "\n\n#use starship as theme" >> $HOME/.zshrc
 echo -e "eval \"$(starship init zsh)\"" >> $HOME/.zshrc
+echo "setting zsh as default shell"
+chsh -s $(which zsh)
+echo "Logout and log back in to have the default shell be zsh"
+exec zsh
